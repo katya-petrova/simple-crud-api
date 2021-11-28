c2d0d76a3bb817fa3bf9556c97862c6044748457
katya-petrova <walki-disi@ya.ru>
2021-11-28 23:14:12 +0300

    refactor: add test scenarios and fix webpack config

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 const server = require("../app");
 const supertest = require("supertest");
 
-describe("Testing simple-crud-api", () => {
+describe("Testing simple-crud-api #1", () => {
   const req = supertest(server);
   let id;
   const mockUser = { name: "Alex", age: 24, hobbies: "coding" };
@@ -37,10 +37,39 @@ describe("Testing simple-crud-api", () => {
     expect(response.status).toBe(204);
   });
 
-  test("GET request with invalid id should return status code 200", async () => {
+  test("GET request with invalid id should return status code 404", async () => {
     const response = await req.get(`/person/${id}`);
     expect(response.status).toBe(404);
   });
 
   server.close();
 });
+
+
+describe('Testing simple-crud-api #2', () => {
+  const req = supertest(server)
+  const mockUser = { name: 'Pete', age: 2 }
+  const nonExistId = '995dd2d8-b918-4eb3-a744-b6374be62089'
+
+  test('GET request with invalid id should return status code 400', async () => {
+    const badId = '!!!efghj'
+    const response = await req.get(`/person/${badId}`)
+    expect(response.status).toBe(400)
+  })
+
+  test('GET request with not existing id should return status code 400', async () => {
+    const response = await req.get(`/person/${nonExistId}`)
+    expect(response.status).toBe(404)
+  })
+})
+
+describe('Testing simple-crud-api #3', () => {
+  const req = supertest(server)
+  const mockUser = { name: 'Garry', age: 65 }
+
+  test('POST request with wrong objects fields should return status code 400', async () => {
+    const response = await req.post('/person').send(mockUser)
+    expect(response.status).toBe(400)
+  })
+})
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  target: "node",
   mode: 'production',
   entry: {
     main: path.resolve(__dirname, "./app.js"),
@@ -13,11 +14,4 @@ module.exports = {
     open: true,
     port: 8080,
   },
-  resolve: {
-    fallback: {
-        "fs": false,
-        "path": false,
-        "os": false,
-    },
-}
 };
